c4ae285ceb54238fa8ba655e04338bc87a5278dd
Skyrdow <lucas.mesias@usach.cl>
2023-06-25 20:37:04 -0400

    final final

--- LAB2/fbroker.h
+++ LAB2/fbroker.h
@@ -1,7 +1,6 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
-#include <sys/wait.h>
 #include <unistd.h>
 
 #define BUFFERMAX 128
--- LAB2/lab2.c
+++ LAB2/lab2.c
@@ -1,6 +1,5 @@
 #include <stdio.h>
 #include <stdlib.h>
-#include <sys/wait.h>
 #include <unistd.h>
 
 #define SMALLBUFFERMAX 16
